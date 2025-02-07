1906aa5499ba905010c5a626135b4b75d67c18fd
Leslie Rojas <leslierojas631@gmail.com>
2025-02-06 19:37:14 -0500

    Añadir funcionalidad de clima con icono y detalles

---
+++ script.js
@@ -0,0 +1,51 @@
+let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+let api_key = '40c4cf1eb005d252326fe622be4415a7'
+let difKelvin = 273.15
+let ciudad = 'Quito'
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {  //Cuando se haga clic
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if (ciudad) {
+        fetchDatosClima(ciudad)
+    }
+})
+
+function fetchDatosClima(ciudad) {
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+        .then(data => data.json())
+        .then(data => mostrarDatosClima(data))
+}
+
+
+function mostrarDatosClima(data) {
+    const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.innerHTML = ''  // Limpiar datos anteriores
+
+    const ciudadNombre = data.name
+    const paisNombre = data.sys.country
+    const temperatura = data.main.temp
+    const humedad = data.main.humidity
+    const descripcion = data.weather[0].description
+    const icono = data.weather[0].icon
+
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura - difKelvin)}°C`
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}°C`
+
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`
+
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(iconoInfo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(descripcionInfo)
+}
